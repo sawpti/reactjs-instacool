268e428bcde4e2e36498ac79f623e58b17eedc33
Hugo Martinez <hmartinez.lagos@gmail.com>
2019-08-26 09:06:37 -0400

    user

--- src/ducks/Users.ts
+++ src/ducks/Users.ts
@@ -2,20 +2,30 @@ import { Dispatch } from 'redux'
 import{ IServices } from '../service'
 
 export interface ILogin{
-    email:string,
+    email:string
     password:string
 }
 export default function reducer(state={}){
     return state 
 }
- // export const login = (payload:ILogin)=>
+// export const login = (payload:ILogin)=>
 export const login = ({email, password}:ILogin)=>
-async  (dispatch: Dispatch, getState:()=>any, { auth }: IServices )  => {
-
-    console.log("auth clg", auth);
-  // const result= await auth.signInWithEmailAndPassword(payload.email, payload.password)
-   const result= await auth.signInWithEmailAndPassword (email, password)
-    console.log(result);
+async  (dispatch: Dispatch, getState:()=>any, servicios: IServices )  => {
+ //  const result= await auth1.signInWithEmailAndPassword(payload.email, payload.password)
+   await servicios.auth.signInWithEmailAndPassword(email, password)
+ // tslint:disable-next-line:no-console
+  //  console.log (" result :::: ",result);
  
 
+}
+
+export const register = ({email,password}: ILogin) => 
+async (dispatch:Dispatch, getstate: () => any, {auth, db }: IServices)=>{
+    const userCredential = await auth.createUserWithEmailAndPassword(email, password)
+  
+    const { user } = userCredential
+    // console.log(user);
+    const id = user ? user.uid : undefined
+    const doc = db.collection('users').doc(id)
+    await doc.set({role: 'user'})
 }
