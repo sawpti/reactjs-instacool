e4b4dfd9877aace2ca3faa74273fbe657a391073
Hugo Martinez <hmartinez.lagos@gmail.com>
2019-08-26 09:04:55 -0400

    user

--- src/ducks/Users.ts
+++ src/ducks/Users.ts
@@ -12,10 +12,10 @@ export default function reducer(state={}){
 export const login = ({email, password}:ILogin)=>
 async  (dispatch: Dispatch, getState:()=>any, { auth }: IServices )  => {
 
-    console.log("auth clg "+auth);
-//  const result= await auth.signInWithEmailAndPassword(payload.email, payload.password)
-// const result= await auth.signInWithEmailAndPassword(email, password)
- // console.log(result);
+    console.log("auth clg", auth);
+  // const result= await auth.signInWithEmailAndPassword(payload.email, payload.password)
+   const result= await auth.signInWithEmailAndPassword (email, password)
+    console.log(result);
  
 
 }
